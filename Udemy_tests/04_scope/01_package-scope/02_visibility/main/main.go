4994839b334df1bd3f9e8aa9845a3fe7bff20b6a
pdus <fe1133aj@gmail.com>
2018-05-19 20:28:16 +0200

    changes in github directory on my computer

--- Udemy_tests/04_scope/01_package-scope/02_visibility/main/main.go
+++ Udemy_tests/04_scope/01_package-scope/02_visibility/main/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/04_scope/01_package-scope/02_visibility/vis"
+
+	"github.com/GolangTests/Udemy_tests/04_scope/01_package-scope/02_visibility/vis"
 )
 
 func main() {
